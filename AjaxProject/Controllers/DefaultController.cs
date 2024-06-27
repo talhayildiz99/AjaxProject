8e77ee88c58a619b8541d9f3892f4a85c4a8aff7
Talha <99talhayildiz@gmail.com>
2024-06-27 21:22:36 +0300

    Finish the Ajax Project

--- AjaxProject/Controllers/DefaultController.cs
+++ AjaxProject/Controllers/DefaultController.cs
@@ -31,5 +31,30 @@ namespace AjaxProject.Controllers
             var values = JsonConvert.SerializeObject(product);
             return Json(values);
         }
+
+        public IActionResult DeleteProduct (int id)
+        {
+            var value = _context.Products.Find(id);
+            _context.Products.Remove(value);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        public IActionResult GetProduct(int id)
+        {
+            var value = _context.Products.Find(id);
+            var jsonValues = JsonConvert.SerializeObject(value);
+            return Json(jsonValues);
+        }
+        public IActionResult UpdateProduct(Product product)
+        {
+            //var value = _context.Products.Find(product.ProductId);
+            //value.ProductName = product.ProductName;
+            //value.Stock = product.Stock;
+            //value.Price = product.Price;
+            _context.Products.Update(product);
+            _context.SaveChanges();
+            return NoContent();
+        }
     }
 }
